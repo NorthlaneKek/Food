bc85200fecdfd18643a220ed1824fe310613424a
nik709 <Nikitagrechukhin@gmail.com>
2016-04-19 21:38:51 +0300

    First commit

---
+++ app/src/main/java/com/example/makpro/recipedesign1/staticString.java
@@ -0,0 +1,10 @@
+package com.example.makpro.recipedesign1;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Никита on 16.04.2016.
+ */
+public class staticString {
+    public static ArrayList<String> str;
+}
